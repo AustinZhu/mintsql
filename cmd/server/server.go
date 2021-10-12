f0103da1a644c9112deca2527040f51ca41bf049
AustinZhu <42071208+AustinZhu@users.noreply.github.com>
2021-10-13 00:04:18 +0900

    net: add connection pool

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -1,1 +1,56 @@
 package server
+
+import (
+	"context"
+	"log"
+	"mintsql/internal/backend"
+	"net"
+)
+
+const (
+	HOST     = "127.0.0.1"
+	PROTOCOL = "tcp"
+	PORT     = 7384
+)
+
+type Server struct {
+	Addr   *net.TCPAddr
+	Engine *backend.Engine
+}
+
+func New(host string, port uint16, poolSize int) (s *Server) {
+	// TODO server network options
+	s = &Server{}
+	s.Addr = &net.TCPAddr{
+		IP:   net.ParseIP(HOST),
+		Port: PORT,
+	}
+	return s
+}
+
+func (s *Server) Run() {
+	log.Printf("Welcome to mintsql Server.")
+	l, err := net.ListenTCP(PROTOCOL, s.Addr)
+	if err != nil {
+		log.Fatal("error listening: ", err)
+	}
+	defer func(l *net.TCPListener) {
+		err := l.Close()
+		if err != nil {
+			log.Println(err)
+		}
+	}(l)
+	log.Printf("Listening on %s", s.Addr)
+	for {
+		conn, err := l.AcceptTCP()
+		log.Printf("Accepted incoming connection on %s", conn.RemoteAddr())
+		if err != nil {
+			log.Fatalln(err)
+		}
+		go s.Handle(context.TODO(), conn)
+	}
+}
+
+func (s *Server) Handle(ctx context.Context, conn *net.TCPConn) {
+	panic("Not Implemented")
+}
--- pkg/mintsql/conn.go
+++ pkg/mintsql/conn.go
@@ -1,1 +1,75 @@
 package mintsql
+
+import (
+	"errors"
+	"fmt"
+	"net"
+)
+
+type Conn struct {
+	net.Conn
+	pool *ConnPool
+}
+
+func (c *Conn) Close() error {
+	select {
+	case c.pool.conn <- c:
+		return nil
+	default:
+		return c.Conn.Close()
+	}
+}
+
+type ConnPool struct {
+	conn chan net.Conn
+	addr *net.TCPAddr
+}
+
+func (cp *ConnPool) Get() (*Conn, error) {
+	if cp.conn == nil {
+		return nil, errors.New("pool closed")
+	}
+	poolConn := &Conn{
+		pool: cp,
+	}
+	select {
+	case c, ok := <-cp.conn:
+		if !ok {
+			return nil, errors.New("failed to get connection")
+		}
+		poolConn.Conn = c
+		return poolConn, nil
+	default:
+		conn, err := net.DialTCP("tcp", nil, cp.addr)
+		if err != nil {
+			return nil, fmt.Errorf("failed to dial %s", cp.addr)
+		}
+		cp.conn <- conn
+		poolConn.Conn = conn
+		return poolConn, nil
+	}
+}
+
+func (cp *ConnPool) Close() {
+	cp.conn = nil
+}
+
+func (cp *ConnPool) Len() int {
+	return len(cp.conn)
+}
+
+func NewConnPool(capacity int, addr *net.TCPAddr) (*ConnPool, error) {
+	if capacity <= 0 {
+		return nil, errors.New("invalid capacity")
+	}
+	cp := &ConnPool{conn: make(chan net.Conn, capacity), addr: addr}
+
+	for i := 0; i < capacity; i++ {
+		conn, err := net.DialTCP("tcp", nil, addr)
+		if err != nil {
+			return nil, fmt.Errorf("failed to dial %s", addr)
+		}
+		cp.conn <- conn
+	}
+	return cp, nil
+}
